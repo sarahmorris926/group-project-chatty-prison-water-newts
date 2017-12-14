690c1205a2b73cf1d755d5d6d710272496b37b61
Sarah Morris <sarah.morris9@gmail.com>
2017-12-14 11:05:20 -0600

    Added minimum dark theme option

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,3 +2,29 @@
 const initialMsgs = require('./msgData');
 const deleteMsg = require('./deleteMsgs');
 const domInput = require('./outputData');
+const darkTheme = document.getElementById('darkTheme');
+let container = document.querySelector('#container');
+
+// Dark Theme Event Listener
+
+darkTheme.addEventListener("click", checked);
+
+  function checked() {
+    if (darkTheme.checked) {
+      console.log("button is checked");
+      container.classList.add("darkTheme");
+      // container.style = 'background-color: black; color: white';
+
+    } else {
+      console.log("not checked");
+      container.classList.remove("darkTheme");
+      // container.style = 'background-color: white; color: black;';
+    }
+  }
+  
+
+
+
+
+
+
